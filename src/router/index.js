de320c5c422f1ca68f1752b07b5a39d9e1aa22ce
liuzhichao163 <44563676+liuzhichao163@users.noreply.github.com>
2018-12-05 12:41:59 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,6 @@ Vue.use(Router)
 const router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: Login
-    },
-    {
       path: '/login',
       name: 'Login',
       component: Login
